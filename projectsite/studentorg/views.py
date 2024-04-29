f2cd6756e8f5fd77ab9b6a430c46a90c5e30f179
KenRandolfMapa <2011880166@psu.palawan.edu.ph>
2024-04-29 09:09:49 +0800

    dynamic piec chart

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 
+from django.db import connection
+from django.http import JsonResponse
+from django.db.models.functions import ExtractMonth
+
+from django.db.models import Count
+from datetime import datetime
+
 
 from django.views.generic.edit  import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization,OrgMember, Student,Program, College
@@ -142,10 +149,27 @@ def forms_view(request):
 
 class ChartView(ListView):
     template_name = 'chart.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
     
     def get_queryset(self,*args,**kwargs):
         pass
+
+def PieCountbySeverity(request):
+    query = '''
+    SELECT severity_level, COUNT(*) as count
+    FROM fire_incident
+    GROUP BY severity_level
+    '''
+    data = {}
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+    if rows:
+        data = {severity: count for severity, count in rows}
+    else:
+        data = {}
+    return JsonResponse(data)
